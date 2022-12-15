a7faa2b91e134df9a894b6ab91afaab6a9987ecc
Alex <mkumum12@mail.ru>
2022-12-15 23:59:46 +0300

    Мини-рефакторинг

--- burger.js
+++ burger.js
@@ -1,68 +1,73 @@
-
-// Распознование устройства 
-var isMobile = {
+document.addEventListener('DOMContentLoaded', () => {
+  // Распознование устройства 
+  var isMobile = {
     Android: function () {
-        return navigator.userAgent.match(/Android/i);
+      return navigator.userAgent.match(/Android/i);
     },
     BlackBerry: function () {
-        return navigator.userAgent.match(/BlackBerry/i);
+      return navigator.userAgent.match(/BlackBerry/i);
     },
     iOS: function () {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
     },
     Opera: function () {
-        return navigator.userAgent.match(/Opera Mini/i);
+      return navigator.userAgent.match(/Opera Mini/i);
     },
     Windows: function () {
-        return navigator.userAgent.match(/IEMobile/i);
+      return navigator.userAgent.match(/IEMobile/i);
     },
     any: function () {
-        return (
-                isMobile.Android()
-                || isMobile.BlackBerry()
-                || isMobile.iOS()
-                || isMobile.Opera()
-                || isMobile.Windows()
-                );
+      return (
+        isMobile.Android()
+        || isMobile.BlackBerry()
+        || isMobile.iOS()
+        || isMobile.Opera()
+        || isMobile.Windows()
+      );
     }
-};
+  };
 
-// Подменю со стрелкой 
-if (isMobile.any()) {
-    document.body.classList.add ('_touch');
+  // Подменю со стрелкой 
+  if (isMobile.any()) {
+    document.body.classList.add('_touch');
 
-let menuArrows = document.querySelectorAll ('.menu_arrow')
-if (menuArrows.length > 0) {
-    for (let index = 0; index < menuArrows.length; index++) {
-        const menuArrow = menuArrows[index]; 
-        menuArrow.addEventListener ("click", function (e) {
-            menuArrow.parentElement.classList.toggle('_active');
+    let menuArrows = document.querySelectorAll('.menu_arrow')
+
+    if (menuArrows.length) {
+      Array.from(menuArrows).forEach(menuArrow => {
+        menuArrow.addEventListener('click', () => {
+          menuArrow.parentElement.classList.toggle('_active');
         });
+      })
     }
-}
-}
-else {
-    document.body.classList.add ('_pc');
-}
+  } else {
+    document.body.classList.add('_pc');
+  }
 
-// плавный переход по сайту
-const menuLinks = document.querySelectorAll('.menu_link[data-goto]');
-if (menuLinks.length > 0) {
-    menuLinks.forEach(menuLink =>{
-        menuLink.addEventListener ("click", onMenuLinkClick);
+  // плавный переход по сайту
+  const menuLinks = document.querySelectorAll('.menu_link[data-goto]');
+
+  if (menuLinks.length) {
+    Array.from(menuLinks).forEach(menuLink => {
+      menuLink.addEventListener('click', onMenuLinkClick);
     });
 
-    function onMenuLinkClick (e) {
-        const menuLink = e.target;
-        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-            const gotoBlock = document.querySelector(menuLink.dataset.goto);
-            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('wrapper').offsetHeight;
+    function onMenuLinkClick (event) {
+      event.preventDefault();
 
-            window.scrollTo({
-                top: gotoBlockValue,
-                behavior: "smooth"
-            });
-            e.preventDefault ();
-        }
+      const menuLink = event.target;
+      const gotoDataset = menuLink.dataset.goto;
+      const gotoBlock = document.querySelector(gotoDataset);
+
+      if (gotoDataset && gotoBlock) {
+        const wrapperOffsetHeight = document.querySelector('.wrapper')
+        const gotoBlockValue = gotoBlock.getBoundingClientRect().top + window.pageYOffset - wrapperOffsetHeight;
+
+        window.scrollTo({
+          top: gotoBlockValue,
+          behavior: 'smooth'
+        });
+      }
     }
-}
+  }
+});
